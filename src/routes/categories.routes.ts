42f87e104c972c2bd286ee4c7ac004df180e5efb
Philippo Rodrigues <philipporodrigues@Philippos-MacBook-Pro.local>
2021-11-09 23:37:19 -0300

    Criando serviço Categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,13 +9,11 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoriesService = new CreateCategoryService(
+    categoriesRepository
+  );
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists" });
-  }
-
-  categoriesRepository.create({ name, description });
+  createCategoriesService.execute({ description, name });
 
   return response.status(201).send();
 });
