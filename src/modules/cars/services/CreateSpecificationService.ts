dc6687837388f13f4b45c49f0f4fee9ac176da17
PhilippoRodrigues <philippo.rodrigues@al.infnet.edu.br>
2021-12-01 23:37:06 -0300

    Implementando Specifications

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,20 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("TODO");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IRequest) {
+    const specificationsAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationsAlreadyExists)
+      throw new Error("Specification already exists");
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
